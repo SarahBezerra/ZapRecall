b018ca12ba1b287be2aaeea7e3219b2850db29c4
Sarah <sarahbezerra2013@gmail.com>
2021-12-09 02:12:18 -0300

    criando Flashcard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Flashcards from "./components/Flashcards";
 import Home from "./components/Home";
 
 export default function App() {
     return(
-        <Home />
+        <Flashcards />
     );
 }
--- src/components/Flashcards.js
+++ src/components/Flashcards.js
@@ -1,5 +1,18 @@
 export default function Flashcards() {
     return(
-        <h1>teste</h1>
+        <>
+        <header className="topo">
+            <img src="./assets/logo-mini.png" className="logo-mini"></img>
+        </header>
+
+        <div className="corpo">
+            <div className="flashcard">
+                <div className="level">1/8</div>
+                <div className="question">Pergunta</div>
+                <div className="turn"><img src="./assets/turn.png" className="icon-turn"></img></div>
+            </div>
+        </div>
+
+        </>
     );
 }
