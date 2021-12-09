e3db042ce7178c663eec3fd50fb4f936db4563cb
Sarah <sarahbezerra2013@gmail.com>
2021-12-09 01:06:56 -0300

    criando Home

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Home from "./components/Home";
+
+export default function App() {
+    return(
+        <Home />
+    );
+}
---
+++ src/components/Flashcards.js
@@ -0,0 +1,5 @@
+export default function Flashcards() {
+    return(
+        <h1>teste</h1>
+    );
+}
